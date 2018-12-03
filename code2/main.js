b2aa32792de6bfcffe7efe11b1d326b4da047833
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2018-12-03 17:14:03 +0900

    [add] テストコード記述

--- code2/main.js
+++ code2/main.js
@@ -1,6 +1,10 @@
-var app = new Vue ({
+new Vue({
     el: '#app',
     data: {
-        show: true
+        message: {
+            value: 'Vim'
+        },
+        list: ['apple', 'banana', 'pine'],
+        num: 1
     }
 })
