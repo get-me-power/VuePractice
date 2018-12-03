e4802f0ab430d737e61effa03eb17fa6af565e99
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2018-12-03 17:32:40 +0900

    [add] テストコードを記述

---
+++ code3/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        count: 0
+    },
+    methods: {
+        //ボタンをクリックしたときのハンドラ
+        increment: function() {
+            this.count += 1
+        }
+    }
+})
