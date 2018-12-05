6890ac8d0881e0acc8bde4d21d542977d0890703
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2018-12-05 22:07:58 +0900

    [add] 新しいテストコードを記載

---
+++ codes/code10/main.js
@@ -0,0 +1,30 @@
+new Vue({
+    el: '#app',
+    data: {
+        name: 'キマイラ',
+        list: [
+            {id: 1, name: 'スライム', hp: 100},
+            {id: 2, name: 'ゴブリン', hp: 100},
+            {id: 3, name: 'ドラゴン', hp: 100}
+        ]
+    },
+    methods: {
+        doAdd () {
+            var max = this.list.reduce( function(a, b) {
+                return a > b.id ? a : b.id 
+            },0)
+
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500
+            })
+        },
+        doRemove: function (index) {
+            this.list.splice(index, 1)
+        },
+        doAttack: function (index) {
+            this.list[index].hp -= 10 //hpを10減らす
+        }
+    }
+})
